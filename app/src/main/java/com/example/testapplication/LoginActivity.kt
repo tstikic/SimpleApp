5c4226dfac0577f11628db8d48d1009c98fcd950
Tijana Stikic <tijana.stikic@pearson.com>
2025-03-20 15:01:17 +0100

    Update Login Activity

--- app/src/main/java/com/example/testapplication/LoginActivity.kt
+++ app/src/main/java/com/example/testapplication/LoginActivity.kt
@@ -4,44 +4,66 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var usernameEditText: EditText
-    private lateinit var passwordEditText: EditText
+    private lateinit var login_button: Button
+    private lateinit var username: EditText
+    private lateinit var password: EditText
     private lateinit var errorMessageTextView: TextView
-    private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        // Initialize UI components
-        usernameEditText = findViewById<EditText>(R.id.username)
-        passwordEditText = findViewById<EditText>(R.id.password)
-        errorMessageTextView = findViewById<TextView>(R.id.error_message)
-        loginButton = findViewById<Button>(R.id.login_button)
+        this.setContentView(R.layout.activity_login)
+        //showHome()
 
-        // Set click listener for login button
-        loginButton.setOnClickListener(View.OnClickListener {
-            val username = usernameEditText.getText().toString()
-            val password = passwordEditText.getText().toString()
+        login_button = findViewById(R.id.login_button)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        errorMessageTextView = findViewById(R.id.error_message)
 
-            // Simple login check
-            if (username == "admin" && password == "1234") {
-                // Login successful, navigate to homepage
-                val intent = Intent(
-                    this@LoginActivity,
-                    HomeActivity::class.java
-                )
-                startActivity(intent)
-                finish()
-            } else {
-                // Show error message
+
+        login_button.setOnClickListener {
+            errorMessageTextView.clearFocus()
+
+
+            if (username.text.toString().isEmpty()) {
                 errorMessageTextView.setVisibility(View.VISIBLE)
-            }
-        })
+                errorMessageTextView.error = "You need to enter username"
+            } else
+                if (password.text.toString().isEmpty()) {
+                    errorMessageTextView.setVisibility(View.VISIBLE)
+                    errorMessageTextView.error = "You need to enter password"
+
+                } else
+                    if (username.text.toString() == "Admin" && password.text.toString() == "123456") {
+                        val intent = Intent(this, HomeActivity::class.java)
+
+                        startActivity(intent)
+                    } else
+                        if (username.text.toString() != "Admin"
+                            || password.text.toString() != "123456"
+                            && password.text.toString().isNotEmpty()
+                            && username.text.toString().isNotEmpty()
+                        ) {
+                            username.text.clear()
+                            password.text.clear()
+                            errorMessageTextView.clearFocus()
+                            errorMessageTextView.setVisibility(View.VISIBLE)
+                            errorMessageTextView.error = "Wrong password or username"
+                        }
+        }
+
     }
+
+
+//    private fun showHome() {
+//        val loginActivity =
+//        R.layout.activity_login = View.VISIBLE
+//        second_layout.visibility = View.GONE
+//    }
 }
