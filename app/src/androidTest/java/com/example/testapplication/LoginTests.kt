5f1be305a76f69f5cf6945e2609694d63fdfc953
Tijana Stikic <tijana.stikic@pearson.com>
2025-03-31 13:00:35 +0200

    Update app

--- app/src/androidTest/java/com/example/testapplication/LoginTests.kt
+++ app/src/androidTest/java/com/example/testapplication/LoginTests.kt
@@ -4,7 +4,6 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withHint
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -16,16 +15,18 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LoginTests {
+    private val username = "Admin"
+    private val password = "123456"
 
     @get:Rule
     var scenarioRule = ActivityScenarioRule(LoginActivity::class.java)
 
     @Test
     fun checkLoginSuccessful() {
-        onView(withId(R.id.username)).perform(typeText("Admin"))
-        onView(withHint("Password:")).perform(typeText("123456"))
-        onView(withText("LOGIN")).perform(click())
-        onView(withId(R.id.welcome_text)).check(matches(withText("Welcome QA Family!")))
+        onView(withId(R.id.username)).perform(typeText(username))
+        onView(withHint(R.string.password_hint_text)).perform(typeText(password))
+        onView(withText(R.string.login_button_text)).perform(click())
+        onView(withId(R.id.welcome_text)).check(matches(withText(R.string.welcome_qa_family)))
     }
 
 }
